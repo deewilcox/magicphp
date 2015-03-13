628085139a1e8d397e2587dc0afd340ad4d6be3b
dwilcox <dwilcox@nasba.org>
2015-03-13 08:28:16 -0500

    Updated magic.js

--- assets/magic.js
+++ assets/magic.js
@@ -4,36 +4,26 @@
     This plugin file contains all of the core methods for MagicPHP utilizing the jQuery 2.x library.
     This library is dependent on the inclusion of the jQuery 2.x library in the web application. jQuery 2.x can
     be accessed by visiting http://jquery.com/download/.
-
-    There core function is defined as magic with options set for default values.
-    Individual methods are defined in addition as functions assigned .
-
  */
 
 (function ( $ ) {
 
-    $.fn.magic = function( options ) {
-        // Extend the default options
-        var opts = $.extend( {}, $.fn.magic.defaults, options );
-    };
-
-    // Defaults are a property of the magic plugin
-    $.fn.magic.defaults = {
-        // Define defaults
+    $.fn.magic = function() {
+        // Initialize the plugin
     };
 
     // Define alert function
-    $.fn.magic.alert = function( text ) {
+    $.fn.magicAlert = function( text ) {
         return alert(text);
     };
 
-    $.fun.magic.alertDialog = function ( title,text, button) {
+    $.fn.magicAlertDialog = function ( title,text, button) {
         // Update to use jQuery UI and tell this function to accept title, text, and button parameters.
     };
 
     // Define assign function
-    $.fn.magic.assign = function( elementId, htmlProperty, data ) {
-        var element = jQuery("#" + elementId + "");
+    $.fn.magicAssign = function( elementId, data ) {
+        var element = $('#' + elementId);
         if( typeof elementId == undefined) {
             debug('assign','Element is undefined');
         }
@@ -41,21 +31,43 @@
             debug('assign','Element is null');
         }
         else {
-            //htmlProperty will most commonly be innerHTML or outerHTML
-            elementId.htmlProperty = data;
+            $('#' + elementId).html(data);
         }
+    };
 
-        return this;
+    // Define callFunction function
+    // @TODO: This method still needs work
+    $.fn.magicCallFunction = function ( functionName, formValues ){
+        console.log(functionName);
+        console.log(formValues);
+        $.ajax({
+            url: "/",
+            data: {
+                requestFunction:functionName,
+                requestParameters: formValues
+            },
+            type: "POST",
+            dataType: "json",
+            success: function(data) {
+                // do something
+            },
+            error: function (data) {
+                console.log(data);
+            },
+            complete: function(data){
+                // do something
+            }
+        });
     };
 
     // Define getFormValues function
-    $.fn.magic.getFormValues = function( formId ) {
-        return jQuery(formId).serializeArray();
+    $.fn.magicGetFormValues = function( formId ) {
+        return $(formId).serializeArray();
     };
 
-    // Define private functions here
+    // Private debugging function
     function debug( functionName, error) {
         console.log('MagicPHP Error: ' + functionName + error);
     }
 
-})( jQuery);
+})( jQuery); // Note: we use jQuery here to avoid conflicts with other plugins.
